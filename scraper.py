8e1454bd28bed434beb69dc67f72f44012480b17
LysandreTerrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2025-10-20 02:00:35 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,105 +1,66 @@
-# python -m venv my-venv
-# my-venv/bin/pip install selenium
-# source my-venv/bin/activate
-# python -m pip install selenium
-# python -m pip install webdrivermanger
-
-# python -m webdrivermanager firefox --linkpath /usr/local/bin (doesn't work)
-
-
-#import pandas
-#import urllib
-#from urllib import request
-
-
-"""
-from selenium import webdriver
+import requests
+from yt_dlp import YoutubeDL
 from bs4 import BeautifulSoup
+ 
+#Wow trop cool
+#https://www.scraperapi.com/web-scraping/youtube/
+ 
 
-driver = webdriver.Chrome()
-driver.get('https://youtube.com')
-"""
-"""
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from bs4 import BeautifulSoup
-import time
+## Extracting YouTube Comments    
+def extract_comments(video_url):
+    opts = {"getcomments": True}
+    with YoutubeDL(opts) as yt:
+        info = yt.extract_info(video_url, download=False)
+        comments = info["comments"]
+        thread_count = info["comment_count"]
+        print("Number of threads: {}".format(thread_count))
+        for comment in comments:
+            print(comment['text'])
+  
 
-binary = FirefoxBinary('/usr/bin/firefox')
-driver = webdriver.Firefox(firefox_binary=binary)
-driver.get('http://example.com')
-"""
-"""
-# Initialize the Selenium WebDriver
-driver = webdriver.Chrome()
+def extract_metadata(url):
+    with YoutubeDL({}) as yt:
+        dico = yt.extract_info(url, download=False)
+        keys = ['id', 'title', 'thumbnail', 'description', 'channel', 'channel_id', 'channel_url', 'duration', 'view_count', 'average_rating', 'age_limit', 'webpage_url', 'categories', 'tags', 'subtitles', 'comment_count', 'like_count', 'channel_follower_count', 'timestamp', 'was_live', 'aspect_ratio']
+        dico = {key:dico[key] for key in dico if key in keys}
+        print("Metadata:", dico)
+        return dico
+  
+## Scraping Channel Information
+def scrape_channel_info(channel_url, api_key):
+    params = {
+        'api_key': api_key,
+        'url': channel_url,
+        'render': 'true'
+    }
+    response = requests.get('https://api.scraperapi.com', params=params)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        channel_name = soup.find('yt-formatted-string', {'id': 'text', "class":"style-scope ytd-channel-name"})
+        channel_desc = soup.find('div', {'id': 'wrapper', "class":"style-scope ytd-channel-tagline-renderer"})
+        if channel_name and channel_desc:
+            channel_info = {
+                "channel_name": channel_name.text.strip(),
+                "channel_desc": channel_desc.text.strip(),
+            }
+            print("Channel Info:", channel_info)
+            return channel_info
+        else:
+            print("Failed to retrieve channel info")
+    else:
+        print("Failed to retrieve the page:", response.status_code)
 
-# Navigate to the target website
-driver.get('http://example.com')
-"""
+# Extract comments
+video_url_for_comments = "https://www.youtube.com/watch?v=Fj1EvQIS1Es"
+extract_comments(video_url_for_comments)
 
-#url = 'https://youtube.com'
-#text = request.urlopen(request.Request(url, headers={"User-Agent": "Mozilla/5.0"})).read()
-#print(text[1:-2])
+print("test")
 
+# Extract metadata
+video_url_for_metadata = "https://www.youtube.com/watch?v=Fj1EvQIS1Es"
+extract_metadata(video_url_for_metadata)
 
-#pip install selenium
-#apt install python3-selenium
-
-
-
-
-# python -m venv my-venv
-# my-venv/bin/pip install selenium
-# source my-venv/bin/activate
-# python -m pip install selenium
-# python -m pip install webdrivermanger
-# python -m pip install --upgrade pip
-
-
-"""
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-
-service = Service(executable_path='/users/Etu7/21501137/Bureau/DALAS/Project/Code/geckodriver')
-options = webdriver.FirefoxOptions()
-driver = webdriver.Firefox(service=service, options=options)
-
-
-
-#driver = webdriver.Firefox(executable_path='~/Bureau/DALAS/Project/Code/geckodriver')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
-"""
-
-"""
-from selenium_firefox import Firefox
-
-ff = Firefox()
-ff.get('https://www.google.com')
-"""
-
-
-
-
-
-"""
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
-binary = FirefoxBinary('/usr/bin/firefox')
-driver = webdriver.Firefox(firefox_binary=binary)
-"""
-
-
-from selenium import webdriver
-driver = webdriver.Firefox()
-
-
+# Scrape channel information
+#api_key = 'YOUR_API_KEY'
+#channel_url = 'https://www.youtube.com/@scraperapi/about'
+#scrape_channel_info(channel_url, api_key)
