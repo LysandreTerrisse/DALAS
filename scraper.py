29b9d77974ee2f1c22843ab13ba18721f883abdb
LysandreTerrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2025-10-23 15:44:28 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,18 @@
 import requests
 from yt_dlp import YoutubeDL
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 # python -m venv my-venv
 # source my-venv/bin/activate
 # python -m pip install yt_dlp
 # python -m pip install requests
 # python -m pip install bs4
+# python -m pip install playwright
+# python -m playwright install
 
-# python -m pip install selenium
-# python -m pip install webdriver-manager
 
-#Inspired from https://www.scraperapi.com/web-scraping/youtube/
+# Inspired from https://www.scraperapi.com/web-scraping/youtube/
 def extract_metadata(url):
     with YoutubeDL({}) as yt:
         dico = yt.extract_info(url, download=False)
@@ -19,17 +20,21 @@ def extract_metadata(url):
         return {key:dico[key] for key in dico if key in keys}
 
 
+# Inspired from https://playwright.dev/python/docs/api/class-browsertype
+def scrape_urls():
+    with sync_playwright() as p:
+        browser = p.firefox.launch_persistent_context("user_data_dir", headless=False)
+        page = browser.new_page()
+        page.goto("https://www.youtube.com/")
+        page.wait_for_selector("a.yt-lockup-metadata-view-model__title", state="attached",  timeout=100000)
+        elements = page.query_selector_all("a.yt-lockup-metadata-view-model__title")
+        urls = ["https://www.youtube.com" + e.get_attribute("href") for e in elements]
+        browser.close()
+        return urls
+
+
+urls = scrape_urls()
+print(len(urls))
+print(urls)
 
 print(extract_metadata("https://www.youtube.com/watch?v=Fj1EvQIS1Es"))
-
-url = 'https://www.youtube.com'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-urls = []
-for link in soup.find_all('a'):
-    print(link.get('href'))
-
-
-#Useful links:
-# https://stackoverflow.com/questions/15512239/python-get-all-youtube-video-urls-of-a-channel
-# https://stackoverflow.com/questions/54973419/scraping-youtube-links-from-a-webpage
